b673fe7a7a3b9bbe310f56ed4a228f38ce8cd011
ykim <ykim2718@gmail.com>
2026-07-08 03:11:32 -0500

    my_flow.py 0.0.19

--- Prefect_Workflow/example/dry_run/my_flow_trigger_x.bat
+++ Prefect_Workflow/example/dry_run/my_flow_trigger_x.bat
@@ -7,5 +7,5 @@ powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0my_flow_trigger.ps1" ^
   -PrefectApiUrl "http://localhost:4200/api" ^
   -PrefectDeployment pipeline/pipelineflow-low ^
   -GitRepo https://github.com/ykim2718/SandBox4Git.git ^
-  -GitCommit 8a6a4265b688fe05047e1aeb50506b79dd928236 ^
+  -GitCommit b3f60a0b3ecb8358d344943ac4589be0154697c7 ^
   -MinioKey electric_power_consumption/v0/powerconsumption.csv
