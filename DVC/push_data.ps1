d82c7025759b199b6942f6516b50352ee731c7c1
ykim <ykim2718@gmail.com>
2026-08-02 10:29:46 -0500

    .

--- DVC/push_data.ps1
+++
@@ -1,7 +0,0 @@
-dvc add data_dir
-git add data_dir.dvc
-git commit -m "Sydney 202605"
-git push origin main
-dvc push
-
-dvc dag
--- DVC/view_data.ps1
+++
@@ -1,4 +0,0 @@
-git log -n 10 --format="%h %cd : %s" --date=format:"%Y-%m-%d %H:%M" -- ':(top)**/*.dvc'
-dvc dag
-dvc dag --mermaid
-dvc dag --dot > pipeline.dot
